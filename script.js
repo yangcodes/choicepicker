851abcdbf58ee8af0239be7441ab1a1ca16434af
Jenny Lin <misslinyang91@gmail.com>
2021-07-09 18:01:54 +1000

    created UI and tags

---
+++ script.js
@@ -0,0 +1,23 @@
+const tagsEl = document.getElementsById("tags");
+const textarea = document.getElementsById("textarea");
+
+textarea.focus();
+
+textarea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+});
+
+function createTags(input) {
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+
+  tagsEl.innerHTML = "";
+  tag.forEach((tag) => {
+    const tagEl = document.createElement("span");
+    tagEl.classList.add("tag");
+    tagEl.innerHTML = tag;
+    tagsEl.appendChild(tagEl);
+  });
+}
